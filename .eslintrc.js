99f30f3b2d8a14438808114d3d11c6d9d6f4d684
Mike Donnalley <mdonnalley@salesforce.com>
2021-05-14 09:52:37 -0600

    feat: add SourceTestkit (#1)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,27 @@
 /*
- * Copyright (c) 2020, salesforce.com, inc.
+ * Copyright (c) 2021, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-// Generated - Do not modify. Controlled by @salesforce/dev-scripts
-// See more at https://github.com/forcedotcom/sfdx-dev-packages/tree/master/packages/dev-scripts
-
 module.exports = {
   extends: ['eslint-config-salesforce-typescript', 'eslint-config-salesforce-license'],
+  // Allow describe and it
+  env: { mocha: true },
   rules: {
-    '@typescript-eslint/restrict-template-expressions': 'off',
+    // Allow assert style expressions. i.e. expect(true).to.be.true
+    'no-unused-expressions': 'off',
+
+    // It is common for tests to stub out method.
+
+    // Return types are defined by the source code. Allows for quick overwrites.
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'jsdoc/check-indentation': 'off',
-    '@typescript-eslint/prefer-regexp-exec': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/camelcase': 'off',
-    'no-underscore-dangle': 'off',
+    // Mocked out the methods that shouldn't do anything in the tests.
+    '@typescript-eslint/no-empty-function': 'off',
+    // Easily return a promise in a mocked method.
     '@typescript-eslint/require-await': 'off',
+
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
 };
