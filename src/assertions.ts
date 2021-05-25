455889e1c0b1d01e98a13d6b9d6fdda3760c5e68
Willie Ruemmele <willieruemmele@gmail.com>
2021-05-24 19:20:32 -0600

    chore: add comment

--- src/assertions.ts
+++ src/assertions.ts
@@ -375,6 +375,7 @@ export class Assertions {
     const toTrack = all.filter((file) => !ignoreFiles.includes(file));
     const membersMap = new Map<string, Set<string>>();
     for (const file of toTrack) {
+      // glob will return files with '/' as separators, this won't work on Windows
       const components = this.metadataResolver.getComponentsFromPath(file.replace(/\//g, path.sep));
       for (const component of components) {
         const metadataType = component.type.name;
